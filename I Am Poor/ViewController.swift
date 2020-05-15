a3c46b72c46326c17ca7f11979c0fe119ddf8632
Ilona Sellenberkova <ilona.sell@gmail.com>
2020-05-15 14:41:33 +0200

    Initialise 2nd Project

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Illy Sell on 15/05/2020.
+//  Copyright © 2020 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
